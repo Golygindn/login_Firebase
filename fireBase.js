378dd5d67ba7957aa7150c327ede27c0956923c5
Golygindn <39010151+Golygindn@users.noreply.github.com>
2020-02-20 01:19:39 +0300

    Add files via upload

---
+++ fireBase.js
@@ -0,0 +1,19 @@
+let app_fireBase = {};
+(function () {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyDD4oeu8V4fGtPXvb_s0-BFadDODJd_psk",
+        authDomain: "test-68c03.firebaseapp.com",
+        databaseURL: "https://test-68c03.firebaseio.com",
+        projectId: "test-68c03",
+        storageBucket: "test-68c03.appspot.com",
+        messagingSenderId: "615981432053",
+        appId: "1:615981432053:web:903b1209ec7a6fbc8519bc",
+        measurementId: "G-NM95G6DTJM"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    app_fireBase = firebase;
+})();
+
