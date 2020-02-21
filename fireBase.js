e70626b704388d2a5a63fcc2e443449442d5abad
danilagolygin <t0j8kzqboh@gmail.com>
2020-02-21 16:40:20 +0300

    Сделал авторизацию

--- fireBase.js
+++ fireBase.js
@@ -1,4 +1,4 @@
-let app_fireBase = {};
+let app_firebase = {};
 (function () {
     // Your web app's Firebase configuration
     const firebaseConfig = {
@@ -14,6 +14,41 @@ let app_fireBase = {};
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
-    app_fireBase = firebase;
+    app_firebase = firebase;
+
+
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    var uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                // User successfully signed in.
+                // Return type determines whether we continue the redirect automatically
+                // or whether we leave that to developer to handle.
+                return true;
+            },
+            uiShown: function() {
+                // The widget is rendered.
+                // Hide the loader.
+                document.getElementById('loader').style.display = 'none';
+            }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>',
+        // Privacy policy url.
+        privacyPolicyUrl: '<your-privacy-policy-url>'
+    };
+    ui.start('#firebaseui-auth-container', uiConfig);
 })();
 
--- login.js
+++ login.js
@@ -1,14 +1,14 @@
 (function () {
     const ui = new firebaseui.auth.AuthUI(firebase.auth());
-    const uiConfig = {
+    var uiConfig = {
         callbacks: {
-            signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
                 // User successfully signed in.
                 // Return type determines whether we continue the redirect automatically
                 // or whether we leave that to developer to handle.
                 return true;
             },
-            uiShown: function () {
+            uiShown: function() {
                 // The widget is rendered.
                 // Hide the loader.
                 document.getElementById('loader').style.display = 'none';
@@ -16,7 +16,7 @@
         },
         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
-        signInSuccessUrl: 'main.html',
+        signInSuccessUrl: '<url-to-redirect-to-on-success>',
         signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -27,14 +27,12 @@
             firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ],
         // Terms of service url.
-        tosUrl: 'main.html>',
+        tosUrl: '<your-tos-url>',
         // Privacy policy url.
-        /*privacyPolicyUrl: '<your-privacy-policy-url>'*/
+        privacyPolicyUrl: '<your-privacy-policy-url>'
     };
-
-    // The start method will wait until the DOM is loaded.
     ui.start('#firebaseui-auth-container', uiConfig);
-})()
+})();
 
 
 
